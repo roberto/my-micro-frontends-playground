508069a2d15ced8d6b6a15d1a3e605617856b4fc
Roberto Soares <roberto.tech@gmail.com>
2018-06-11 09:48:40 -0300

    Add custom element example

---
+++ web-components/example.js
@@ -0,0 +1,17 @@
+// reference:
+// https://medium.com/dev-channel/the-case-for-custom-elements-part-1-65d807b4b439
+
+class Example extends HTMLElement {
+  // This gets called when the HTML parser sees your tag
+  constructor() {
+    super(); // always call super() first in the constructor.
+    this.msg = 'Hello, World!';
+  }
+  // Called when your element is inserted in the DOM or
+  // immediately after the constructor if it’s already in the DOM
+  connectedCallback() {
+    this.innerHTML = `<p>${this.msg}</p>`;
+  }
+}
+// This registers your new tag and associates it with your class
+window.customElements.define('my-example', Example);
