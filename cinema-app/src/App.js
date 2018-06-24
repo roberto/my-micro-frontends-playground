61a2347c56c72db04d12140e364c6f465237cea5
Roberto Soares <roberto.tech@gmail.com>
2018-06-24 16:08:50 -0300

    Add Buma (CSS framework) and create a page with header, content and footer to be used as base

--- cinema-app/src/App.js
+++ cinema-app/src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <React.Fragment>
+	<section className="hero is-info">
+	  <div className="hero-body">
+	    <div className="container">
+	      <h1 className="title">
+		<span className="icon"><i className="fas fa-film"></i></span> Cinema
+	      </h1>
+	      <h2 className="subtitle">
+		Buy your ticket, a candy and pick a seat!
+	      </h2>
+	    </div>
+	  </div>
+	</section>
+	<section className="section">
+	  <div className="container">
+	    <div className="content">
+	      <h1>Dale</h1>
+	      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+	    </div>
+	  </div>
+	</section>
+	<footer className="footer">
+	  <div className="content has-text-centered">
+	    <p>
+		Created by Roberto for demo purposes only.
+	    </p>
+	  </div>
+	</footer>
+      </React.Fragment>
     );
   }
 }
