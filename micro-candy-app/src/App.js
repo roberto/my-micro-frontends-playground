b533ef876bdc9f45f6b26a4b2b5315ff0ada0824
Roberto Soares <roberto.tech@gmail.com>
2018-06-25 18:18:33 -0300

    Add a form into micro-app Candy Store

--- micro-candy-app/src/App.js
+++ micro-candy-app/src/App.js
@@ -1,19 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {pluralize, titleize} from 'inflected';
 import './App.css';
+
+const CandyInput = ({candy}) =>
+  <div className="field is-horizontal">
+    <div className="field-label is-normal">
+      <label className="label">{titleize(candy)}</label>
+    </div>
+    <div className="field-body">
+      <div className="field">
+        <p className="control is-expanded">
+          <div className="select">
+            <select>
+              <option selected>0 {pluralize(candy)}</option>
+              <option>1 {candy}</option>
+              <option>2 {pluralize(candy)}</option>
+              <option>3 {pluralize(candy)}</option>
+            </select>
+          </div>
+        </p>
+      </div>
+    </div>
+  </div>
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section className="section">
+        <div className="content">
+          <h2 className="title">Candy Store</h2>
+          <p>
+            Catch your candies in the exclusive line before your movie session.
+          </p>
+          <CandyInput candy="lolypop"/>
+          <CandyInput candy="chocolate"/>
+        </div>
+        <nav className="level">
+          <div className="level-left">
+          </div>
+
+          <div className="level-right">
+            <p className="level-item">
+              <div className="field is-grouped">
+                <p className="control">
+                  <button className="button">Cancel</button>
+                </p>
+                <p className="control">
+                  <button className="button is-primary">Add to cart</button>
+                </p>
+              </div>
+            </p>
+          </div>
+        </nav>
+      </section>
     );
   }
 }
