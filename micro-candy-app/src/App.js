d5887cdba47e4e27478748b810a0aa80bf128b72
Felipe Machuca <fmachuca.sanjuan@gmail.com>
2018-06-27 17:39:25 -0400

    Adds addToCart event to add selected candies

--- micro-candy-app/src/App.js
+++ micro-candy-app/src/App.js
@@ -11,11 +11,11 @@ const CandyInput = ({candy}) =>
       <div className="field">
         <p className="control is-expanded">
           <div className="select">
-            <select>
-              <option selected>0 {pluralize(candy)}</option>
-              <option>1 {candy}</option>
-              <option>2 {pluralize(candy)}</option>
-              <option>3 {pluralize(candy)}</option>
+            <select name={candy}>
+              <option value="0" selected>0 {pluralize(candy)}</option>
+              <option value="1">1 {candy}</option>
+              <option value="2">2 {pluralize(candy)}</option>
+              <option value="3">3 {pluralize(candy)}</option>
             </select>
           </div>
         </p>
@@ -24,34 +24,48 @@ const CandyInput = ({candy}) =>
   </div>
 
 class App extends Component {
+  addToCart(e) {
+    e.preventDefault();
+
+    const candies = {
+      lolypop: e.target.lolypop.value,
+      chocolate: e.target.chocolate.value
+    };
+
+    window.dispatchEvent(new CustomEvent('addToCart', { detail: candies }));
+    window.dispatchEvent(new CustomEvent('closeApp'));
+  }
+
   render() {
     return (
       <section className="section">
-        <div className="content">
-          <h2 className="title">Candy Store</h2>
-          <p>
-            Catch your candies in the exclusive line before your movie session.
-          </p>
-          <CandyInput candy="lolypop"/>
-          <CandyInput candy="chocolate"/>
-        </div>
-        <nav className="level">
-          <div className="level-left">
-          </div>
-
-          <div className="level-right">
-            <p className="level-item">
-              <div className="field is-grouped">
-                <p className="control">
-                  <button className="button">Cancel</button>
-                </p>
-                <p className="control">
-                  <button className="button is-primary">Add to cart</button>
-                </p>
-              </div>
+        <form onSubmit={this.addToCart}>
+          <div className="content">
+            <h2 className="title">Candy Store</h2>
+            <p>
+              Catch your candies in the exclusive line before your movie session.
             </p>
+            <CandyInput candy="lolypop"/>
+            <CandyInput candy="chocolate"/>
           </div>
-        </nav>
+          <nav className="level">
+            <div className="level-left">
+            </div>
+
+            <div className="level-right">
+              <p className="level-item">
+                <div className="field is-grouped">
+                  <p className="control">
+                    <button className="button">Cancel</button>
+                  </p>
+                  <p className="control">
+                    <button type="submit" className="button is-primary">Add to cart</button>
+                  </p>
+                </div>
+              </p>
+            </div>
+          </nav>
+        </form>
       </section>
     );
   }
