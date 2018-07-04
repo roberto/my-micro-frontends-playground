2d07a1cf07e7542fb5c20c28840cc7c35eb5adfc
Roberto Soares <roberto.tech@gmail.com>
2018-07-03 22:10:21 -0300

    Add fetch data to container, showing loading or list of objects

--- web-components/my-container.js
+++ web-components/my-container.js
@@ -1,17 +1,30 @@
-// reference:
-// https://medium.com/dev-channel/the-case-for-custom-elements-part-1-65d807b4b439
-
 class MyContainer extends HTMLElement {
-  // This gets called when the HTML parser sees your tag
+
   constructor() {
-    super(); // always call super() first in the constructor.
-    this.msg = 'Hello, World!';
+    super();
+    this.loadData();
   }
-  // Called when your element is inserted in the DOM or
-  // immediately after the constructor if it’s already in the DOM
+
+  loadData() {
+    fetch('http://localhost:7002/widgets')
+      .then(response => response.json())
+      .then(data => {
+        this.widgets = data.widgets;
+        this.render();
+      });
+  }
+
   connectedCallback() {
-    this.innerHTML = `<p>${this.msg}</p>`;
+    this.render();
+  }
+
+  render() {
+    if (this.widgets) {
+      this.innerHTML = `<p>${this.widgets}</p>`;
+    } else {
+      this.innerHTML = `<p>Loading...</p>`;
+    }
   }
 }
-// This registers your new tag and associates it with your class
+
 window.customElements.define('my-container', MyContainer);
